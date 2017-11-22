3390b2d4a3d7ea7cecd4f7529cd59668c53cd6b6
Jennifer Haddock <jentx123@hotmail.com>
2017-11-22 22:53:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  let list = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  let list = document.querySelectorAll('#grand-node div');
+  return list[list.length - 1];
+}
