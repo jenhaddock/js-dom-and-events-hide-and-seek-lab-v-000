91b1552554c9c944512693a53426d30ee12b0730
Jennifer Haddock <jentx123@hotmail.com>
2017-11-22 22:22:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.querySelector('#nested .target');
 }
+
+function increaseRankBy(n){
+  const list = document.querySelector('ul.ranked-list li');
+  for (let i = 0; i < list.length, i++){
+    list[i].innerHTML = (parseInt(i) + 1)
+  }
+}
